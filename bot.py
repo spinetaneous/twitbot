2f1963e70682e3954f317848586806cde8be2670
Irene <ireneho.imyh@gmail.com>
2017-08-31 23:04:12 -0700

    OAuth authentication

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-from secrets import *
+import tweepy #twitter API
+from secrets import * #get keys from secrets.py
+
+#OAuth authentication
+auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+#api.update_status("GEE WHIZ I CAN TWEET NOW")
