ec4a3428a6c15a49965c2df2c78f3e5b88f13b5c
Irene <ireneho.imyh@gmail.com>
2017-09-03 02:09:36 -0700

    retrieve and print one tweet from search results

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import tweepy #twitter API
+import re #regex library
 from secrets import * #get keys from secrets.py
 
 #OAuth authentication
@@ -7,3 +8,13 @@ auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 #api.update_status("GEE WHIZ I CAN TWEET NOW")
+
+#find a tweet with a swear word
+#for now, the swear word is hard coded just to test things out
+search_results = api.search("hell", "en", "en", 1)
+
+#create re object so tweets will exclude @'s
+#re_obj = re.compile('[@]')
+
+#prints first tweet
+print(search_results[0].text)
