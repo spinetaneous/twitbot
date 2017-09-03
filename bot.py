0085aed6b8bf4539f8c8648535b41dcf6524dd41
Irene <ireneho.imyh@gmail.com>
2017-09-03 02:50:51 -0700

    put search into a function

--- bot.py
+++ bot.py
@@ -12,6 +12,19 @@ api = tweepy.API(auth)
 #for now, the swear word is hard coded just to test things out
 search_results = api.search("hell", "en", "en", 1)
 
-for tweet in search_results:
-    if '@' not in tweet.text:
-        print(tweet.text)
+"""
+goes through a list of tweets and finds one that isn't a mention
+it's a lonely tweet because it doesn't mention anyone
+this is so that i don't bother anyone while i'm testing the bot
+@return tweet: the original tweet with swear words that will be changed later
+"""
+def find_lonely_tweet(search_results):
+    #go through search_results...
+    for tweet in search_results:
+        if '@' not in tweet.text:
+            #...and find a tweet that isn't a mention
+            return tweet
+
+
+orig_tweet = find_lonely_tweet(search_results)
+print(orig_tweet.text)
