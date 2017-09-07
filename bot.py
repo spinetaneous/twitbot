ca3e5cc3c3c7fa15ccdf397601d0945566dae09d
Irene <ireneho.imyh@gmail.com>
2017-09-07 02:14:07 -0700

    added random replacement word picker

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ def censor(orig):
     for key in cuss.words.keys():
         #check original string for words to replace
         #FIXME only uses first entry in list; should be random later
-        #new = re.sub(key, cuss.words[key][0], orig, flags=re.IGNORECASE)
+        #new = re.sub(key, random.choice(cuss.words[key]), orig, flags=re.IGNORECASE)
         new = re.sub(key, cuss.words[key][0], orig, flags=re.IGNORECASE) #compatible with test suite
         orig = new #reassign orig
         #print(key, cuss.words[key][0], new)
